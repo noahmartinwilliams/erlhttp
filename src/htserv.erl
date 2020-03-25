329bc7f3c61cb91bbc5169758656af6472ef5dac
skynet1 <thecrazymancan@gmail.com>
2020-03-25 12:03:49 -0500

    Added dynamic generation of content.

--- src/htserv.erl
+++ src/htserv.erl
@@ -1,11 +1,13 @@
 -module(htserv).
 
--export([start/0]).
+-export([start/0, do/1]).
+
+do(_) -> {proceed, [{response, {200, "<html><body>hi</body></html>"}}]}.
 
 go() ->
 	go().
 
 start() ->
 	inets:start(),
-	{ok, Pid} = inets:start(httpd, [{port, 8080}, {server_name, "httpd"}, {server_root, "."}, {document_root, "./htdocs"}, {bind_address, "localhost"}]),
+	{ok, Pid} = inets:start(httpd, [{modules, [htserv]}, {port, 8080}, {server_name, "httpd"}, {server_root, "."}, {document_root, "./htdocs"}, {bind_address, "localhost"}]),
 	go().
